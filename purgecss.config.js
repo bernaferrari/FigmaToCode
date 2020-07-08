3222c6ed4830ad78f63845898f809268a4701b4f
Bernardo <bernaferrari2@gmail.com>
2020-07-08 00:25:22 -0300

    Clean up.

--- purgecss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    content: ['public/index.html', 'public/*.js'],
-    css: ['public/global.css'],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-};
