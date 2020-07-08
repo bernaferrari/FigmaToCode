27e8e08dff814a8e4dcb8c89ce71e0579e55dc85
Bernardo <bernaferrari2@gmail.com>
2020-07-08 00:25:22 -0300

    Clean up.

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-// tailwind.config.js
-const defaultTheme = require('tailwindcss/defaultTheme');
-
-module.exports = {
-    // Purge and minification on PostCSS, postcss.config.js
-    purge: false
-}
