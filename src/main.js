6d6d4c480a2b2b126caf43e411c28f109583df42
Burhanuddin Udaipurwala <burhanuddin.u@razorpay.com>
2023-01-19 22:55:45 +0530

    feat: get basic webpack working

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import App from './ui/Main';
+import App from './ui/Main.svelte';
 
 const app = new App({
 	target: document.body,
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ const path = require('path');
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
 
-module.exports = {
-    entry: './src/code.ts',
+const commonConfig = {
     devtool: prod ? false : 'inline-source-map',
     resolve: {
         alias: {
@@ -26,28 +25,28 @@ module.exports = {
                 use: {
                     loader: 'svelte-loader',
                     options: {
-                        emitCss: true,
-                        hotReload: true
+                        // emitCss: true,
+                        // hotReload: true,
+                        preprocess:  require('svelte-preprocess')({})
                     }
                 },
-                exclude: /node_modules/
             },
             {
                 test: /\.css$/,
                 use: [
-					/**
-					 * MiniCssExtractPlugin doesn't support HMR.
-					 * For developing, use 'style-loader' instead.
-					 * */
+                    /**
+                     * MiniCssExtractPlugin doesn't support HMR.
+                     * For developing, use 'style-loader' instead.
+                     * */
                     prod ? MiniCssExtractPlugin.loader : 'style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
             }
         ],
-    },
-    output: {
-        filename: 'code.js',
-        path: path.resolve(__dirname, 'public'),
     },
     mode,
     plugins: [
@@ -56,3 +55,26 @@ module.exports = {
         })
     ],
 };
+
+module.exports = [
+    {
+        ...commonConfig,
+        name: 'code',
+        entry: './src/code.ts',
+        output: {
+            filename: 'code.js',
+            path: path.resolve(__dirname, 'public')
+        },
+    },
+    {
+        ...commonConfig,
+        name: 'ui',
+        entry: './src/main.js',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'src', 'build'),
+            library: 'ui',
+            libraryTarget: 'umd'
+        },
+    },
+];
