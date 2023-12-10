f588cc67382b72d4329720603962a90b124d9606
Tomo Harada <tharada@sonix.asia>
2023-12-10 22:01:45 +0900

    remove gradient because of using @drawable

--- packages/backend/src/android/builderImpl/androidColor.ts
+++ packages/backend/src/android/builderImpl/androidColor.ts
@@ -1,20 +1,9 @@
 import { retrieveTopFill } from "../../common/retrieveFill";
-import { gradientAngle } from "../../common/color";
-import { nearestValue } from "../../tailwind/conversionTables";
-import { sliceNum } from "../../common/numToAutoFixed";
+import { resourceName } from "../androidDefaultBuilder";
 
 export const AndroidSolidColor = (fill: Paint): string => {
   if (fill && fill.type === "SOLID") {
     return androidColor(fill.color, fill.opacity ?? 1.0);
-  } else if (
-    fill &&
-    (fill.type === "GRADIENT_LINEAR" ||
-      fill.type === "GRADIENT_ANGULAR" ||
-      fill.type === "GRADIENT_RADIAL")
-  ) {
-    if (fill.gradientStops.length > 0) {
-      return androidColor(fill.gradientStops[0].color, fill.opacity ?? 1.0);
-    }
   }
 
   return "";
@@ -29,8 +18,6 @@ export const androidSolidColor = (
     // opacity should only be null on set, not on get. But better be prevented.
     const opacity = fill.opacity ?? 1.0;
     return androidColor(fill.color, opacity);
-  } else if (fill?.type === "GRADIENT_LINEAR") {
-    return androidRGBAColor(fill.gradientStops[0].color);
   } else if (fill?.type === "IMAGE") {
     return androidColor(
       {
@@ -55,52 +42,12 @@ export const androidBackground = (
     // opacity should only be null on set, not on get. But better be prevented.
     const opacity = fill.opacity ?? 1.0;
     return androidColor(fill.color, opacity);
-  } else if (fill?.type === "GRADIENT_LINEAR") {
-    return androidGradient(fill);
-  } else if (fill?.type === "IMAGE") {
-    return `AsyncImage(url: URL(string: "https://via.placeholder.com/${node.width.toFixed(
-      0
-    )}x${node.height.toFixed(0)}"))`;
+  } else if (fill?.type === "GRADIENT_LINEAR" || fill?.type === "IMAGE" && node.name) {
+    return `@drawable/${resourceName(node.name)}`;
   }
 
   return "";
 };
-
-export const androidGradient = (fill: GradientPaint): string => {
-  const direction = gradientDirection(gradientAngle(fill));
-
-  const colors = fill.gradientStops
-    .map((d) => {
-      return androidColor(d.color, d.color.a);
-    })
-    .join(", ");
-
-  return `LinearGradient(gradient: Gradient(colors: [${colors}]), ${direction})`;
-};
-
-const gradientDirection = (angle: number): string => {
-  switch (nearestValue(angle, [-180, -135, -90, -45, 0, 45, 90, 135, 180])) {
-    case 0:
-      return "startPoint: .leading, endPoint: .trailing";
-    case 45:
-      return "startPoint: .topLeading, endPoint: .bottomTrailing";
-    case 90:
-      return "startPoint: .top, endPoint: .bottom";
-    case 135:
-      return "startPoint: .topTrailing, endPoint: .bottomLeading";
-    case -45:
-      return "startPoint: .bottomLeading, endPoint: .topTrailing";
-    case -90:
-      return "startPoint: .bottom, endPoint: .top";
-    case -135:
-      return "startPoint: .bottomTrailing, endPoint: .topLeading";
-    default:
-      // 180 and -180
-      return "startPoint: .trailing, endPoint: .leading";
-  }
-};
-
-export const androidRGBAColor = (color: RGBA) => androidColor(color, color.a);
 
 export const androidColor = (color: RGB, opacity: number): string => {
   if (color.r + color.g + color.b === 0 && opacity === 1) {
